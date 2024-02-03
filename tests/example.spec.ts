802a3a82b1cc4c61f7bc199484d49c0b354b2c1d
AKogut <a.kogut01@gmail.com>
2024-02-03 15:57:35 +0200

    small changes

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 const checkTextWritingTestPage = ['Introduction', 'First test', 'Actions', 'Assertions'];
 
-test('first test - check header`s text', async ({page}) => {
+test('test - check header`s text', async ({page}) => {
   await page.goto('https://playwright.dev/');
   await page.getByRole('link', { name: 'Get started' }).click();
   await page.getByRole('link', { name: 'Writing tests', exact: true }).click();
